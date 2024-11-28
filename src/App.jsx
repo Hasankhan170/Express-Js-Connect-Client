153978c69f0bade18d63616b861ca693fa4f9954
Hasankhan170 <hasankhanali0316@gmail.com>
2024-11-28 12:53:15 +0500

    finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,56 @@
-import { useEffect, useState } from "react"
+import axios from "axios"
+import { useEffect, useRef, useState } from "react"
 
 
 const App = () => {
   const [arr,setArr] = useState([])
+  const titVal = useRef()
+  const desVal = useRef()
 
-  const fetchData = async ()=>{
-    try {
-    const response = await fetch("http://localhost:3000/api/v1/todos")
-    const data = await response.json()
-    console.log(data.todos);
-    setArr(data.todos)
-    
-    } catch (error) {
-      console.log(error);
+//get data
+ 
+    const getData = async ()=>{
+      try {
+      const response = await fetch("http://localhost:3000/api/v1/todos")
+      const data = await response.json()
+      // console.log(data.todos);
+      setArr(data.todos)
+      } catch (error) {
+        console.log(error); 
+      }
+    }
+    getData()
+
+
+
+//post data  
+  
+    const postData = async (event)=>{
+      event.preventDefault()
+      try {
+      const response  = await axios.post("http://localhost:3000/api/v1/todo",
+      {
+        title : titVal.current.value,
+        description : desVal.current.value
+      })
+      setArr(response.todos)
+      console.log(response.data);
+      getData()
+      } catch (error) {
+        console.log(error);
+        
+      }
       
     }
-  
-    
-
-  }
-  useEffect(()=>{
-    fetchData()
-  },[])
   
   return (
     <>
     <h1>Todo App</h1>
+     <form onSubmit={postData}>
+      <input type="text" ref={titVal} placeholder="enter title" />
+      <input type="text" ref={desVal} placeholder="enter description" />
+      <button type="submit">Add</button>
+    </form> 
     {
       arr ? arr.map((item)=>{
         return <div key={item._id}>
